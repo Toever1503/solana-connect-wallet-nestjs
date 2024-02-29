efdd919d6f238f4584033482ec55a6d5188088df
Kai <haunv1503@gmail.com>
2024-02-29 17:02:58 +0700

    update: solana wallet base

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,30 @@
-import { useTranslations } from "next-intl";
+"use client"
+import useUserSOLBalanceStore from "@/stores/useUserSOLBalanceStore";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+// import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const t = useTranslations('Index');
-  return <h1>{t('title')} - home page</h1>;
+  // const t = useTranslations('Index');
+
+  const wallet = useWallet();
+  const { connection } = useConnection();
+
+  const balance = useUserSOLBalanceStore((s) => s?.balance)
+  const { getUserSOLBalance } = useUserSOLBalanceStore()
+
+  const [balanceUser, setBalanceUser] = useState<number>(0);
+  useEffect(() => {
+    if (wallet.publicKey) {
+      console.log(wallet.publicKey.toBase58())
+      getUserSOLBalance(wallet.publicKey, connection).then(val => {
+        setBalanceUser(val)
+      })
+    }
+  }, [wallet.publicKey, connection, getUserSOLBalance])
+
+  return <h1>
+
+    Your wallet balance:  {(balanceUser || 0).toLocaleString()}
+  </h1>;
 }
--- app/[locale]/tour/page.tsx
+++
@@ -1,6 +0,0 @@
-import { useTranslations } from "next-intl";
-
-export default function Home() {
-  const t = useTranslations('Index');
-  return <h1>{t('title')} - tour page</h1>;
-}
